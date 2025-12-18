7468eed42707d2db0fd40f4f18fce1525afffc9d
lube-blue <blueriser12@gmail.com>
2025-12-18 11:57:59 +0700

    Update App.js CSS/Inline

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react'
 
 function App() {
-  return (
-    <table border="1">
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-      </tr>
-      <tr>
-        <td>React</td>
-        <td>320</td>
-      </tr>
-      <tr>
-        <td>React Native</td>
-        <td>340</td>
-      </tr>
-    </table>
-  )
-}
+  const divStyle = {
+  color: 'red',
+  backgroundColor: 'powderblue',
+  fontSize: 'larger',
+  padding: '3px',
+  }
+  return <div style={divStyle}>Hello React</div>
+  }
+  
 export default App;
