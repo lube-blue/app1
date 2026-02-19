f4735a38a5579738731d4f01d9a5971cf3d21244
lube-blue <85723519+lube-blue@users.noreply.github.com>
2026-02-19 11:47:02 +0700

    Input text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react'
 import Router3 from './router3'; // เปลี่ยนเป็น Router3 ตามโค้ดที่คุณเขียนล่าสุด
-
+import FormText from './form-text';
 export default function App() {
-  return <Router3 />
+ 
+  return <FormText />
 }
