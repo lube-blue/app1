d9d199b63be01ec39303c0a4f8dd8c7069603898
lube-blue <blueriser12@gmail.com>
2026-02-26 10:29:13 +0700

    SelecFile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import './App.css';
 import React from 'react'
-import Router3 from './router3'; // เปลี่ยนเป็น Router3 ตามโค้ดที่คุณเขียนล่าสุด
-import FormText from './form-text';
+import FormSelectFile from './form-select-file';
 export default function App() {
- 
-  return <FormText />
+return <FormSelectFile/>
 }
